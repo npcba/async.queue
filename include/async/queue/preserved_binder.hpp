b248e195ddc8285a230118c37903989e00bd285c
Victor Scherba <SherbaVA@pba.su>
2023-10-19 11:41:23 +0300

    New async queue header-only library.

---
+++ include/async/queue/preserved_binder.hpp
@@ -0,0 +1,115 @@
+#pragma once
+#include <tuple>
+#include <utility>
+#include <type_traits>
+
+#include <boost/asio/associated_allocator.hpp>
+#include <boost/asio/associated_executor.hpp>
+#include <boost/asio/handler_continuation_hook.hpp>
+
+
+namespace ba {
+namespace async {
+
+template<typename F, typename Tuple, std::size_t... I>
+constexpr decltype(auto)
+apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
+{
+    return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...);
+}
+
+template <typename F, typename Tuple>
+constexpr decltype(auto)
+apply(F&& f, Tuple&& t)
+{
+    return apply_impl(std::forward<F>(f), std::forward<Tuple>(t),
+        std::make_index_sequence<
+        std::tuple_size_v<std::decay_t<Tuple>>>{});
+}
+
+template <typename F, typename... Args>
+class PreservedBinder
+{
+public:
+    explicit PreservedBinder(F f, Args... args)
+        : m_f{ f }, m_args{ args... }
+    {
+    }
+
+    auto operator()()
+    {
+        return apply(m_f, m_args);
+    }
+
+    F f() const
+    {
+        return m_f;
+    }
+
+private:
+    F m_f;
+    std::tuple<std::decay_t<Args>...> m_args;
+};
+
+template <typename F, typename... Args>
+auto preservedBind(F f, Args... args)
+{
+    return PreservedBinder<F, Args...>{ f, args... };
+}
+
+} // namespace async
+} // namespace ba
+
+// Calls to asio_handler_is_continuation must be made from a namespace that
+// does not contain overloads of this function. This namespace is defined here
+// for that purpose.
+namespace handler_cont_helpers {
+
+template <typename Context>
+inline bool is_continuation(Context& context)
+{
+  using boost::asio::asio_handler_is_continuation;
+  return asio_handler_is_continuation(
+      boost::asio::detail::addressof(context));
+}
+
+}
+
+namespace boost {
+namespace asio {
+
+template <typename F, typename... Args, typename Executor>
+struct associated_executor<PreservedBinder<F, Args...>, Executor>
+{
+  typedef typename associated_executor<F, Executor>::type type;
+
+  static type get(const PreservedBinder<F, Args...>& b,
+      const Executor& ex = Executor()) BOOST_ASIO_NOEXCEPT
+  {
+    return associated_executor<F, Executor>::get(b.f(), ex);
+  }
+};
+
+template <typename F, typename... Args, typename Allocator>
+struct associated_allocator<PreservedBinder<F, Args...>, Allocator>
+{
+  typedef typename associated_allocator<F, Allocator>::type type;
+
+  static type get(const PreservedBinder<F, Args...>& b,
+      const Allocator& a = Allocator()) BOOST_ASIO_NOEXCEPT
+  {
+    return associated_allocator<F, Allocator>::get(b.f(), a);
+  }
+};
+
+template <typename F, typename... Args>
+inline bool asio_handler_is_continuation(
+    PreservedBinder<F, Args...>* this_handler)
+{
+  return handler_cont_helpers::is_continuation(
+      this_handler->f());
+}
+
+} //namespace asio
+} //namespace boost
+
---
+++ tests/queue/test.cpp
@@ -0,0 +1,279 @@
+#define BOOST_TEST_MODULE Libasync
+
+#include <async/queue/queue.hpp>
+
+#include <iostream>
+#include <thread>
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/use_future.hpp>
+#define BOOST_COROUTINES_NO_DEPRECATION_WARNING
+#include <boost/asio/spawn.hpp>
+
+
+class ThreadPool
+{
+public:
+    ThreadPool(boost::asio::io_context&ioc, std::size_t n)
+    {
+        while (n--)
+            m_threads.emplace_back([&ioc]() { ioc.run(); });
+    }
+
+    void join()
+    {
+        for (auto& th : m_threads)
+            th.join();
+    }
+
+private:
+    std::vector<std::thread> m_threads;
+};
+
+BOOST_AUTO_TEST_CASE(strandTest)
+{
+    boost::asio::io_context ioc;
+    boost::asio::strand<boost::asio::io_context::executor_type> s1{ ioc.get_executor() };
+    boost::asio::io_context::strand s2{ ioc };
+
+    ba::async::Queue<int> q0{ ioc, 10 };
+
+    q0.asyncPush(123, boost::asio::bind_executor(s1, [s1](boost::system::error_code ec) {
+        BOOST_CHECK(s1.running_in_this_thread());
+        }));
+
+    q0.asyncPop(boost::asio::bind_executor(s1, [s1](boost::system::error_code ec, boost::optional<int> val) {
+        BOOST_CHECK(s1.running_in_this_thread());
+        BOOST_CHECK(123 == *val);
+        }));
+
+    ThreadPool{ ioc, 10 }.join();
+
+    q0.asyncPush(123, s2.wrap([s2](boost::system::error_code ec) {
+        BOOST_CHECK(s2.running_in_this_thread());
+        }));
+
+    q0.asyncPop(s2.wrap([s2](boost::system::error_code ec, boost::optional<int> val) {
+        BOOST_CHECK(s2.running_in_this_thread());
+        BOOST_CHECK(123 == *val);
+        }));
+
+    ioc.restart();
+    ThreadPool{ ioc, 10 }.join();
+
+    ba::async::Queue<int> q1{ s1, 10 };
+
+    q1.asyncPop([s1](boost::system::error_code ec, boost::optional<int> val) {
+        BOOST_CHECK(s1.running_in_this_thread());
+        BOOST_CHECK(123 == *val);
+        });
+
+    q1.asyncPush(123, [s1](boost::system::error_code ec) {
+        BOOST_CHECK(s1.running_in_this_thread());
+        });
+
+    ioc.restart();
+    ThreadPool{ ioc, 10 }.join();
+
+    ba::async::Queue<int> q2{ s2, 10 };
+
+    q2.asyncPop([s2](boost::system::error_code ec, boost::optional<int> val) {
+        BOOST_CHECK(s2.running_in_this_thread());
+        BOOST_CHECK(123 == *val);
+        });
+
+    q2.asyncPush(123, [s2](boost::system::error_code ec) {
+        BOOST_CHECK(s2.running_in_this_thread());
+        });
+
+    ioc.restart();
+    ThreadPool{ ioc, 10 }.join();
+
+    BOOST_CHECK(q0.empty());
+    BOOST_CHECK(q1.empty());
+    BOOST_CHECK(q2.empty());
+
+    BOOST_CHECK_EQUAL(0, q0.cancel());
+    BOOST_CHECK_EQUAL(0, q1.cancel());
+    BOOST_CHECK_EQUAL(0, q2.cancel());
+}
+
+BOOST_AUTO_TEST_CASE(futureTest)
+{
+    boost::asio::io_context ioc;
+    ba::async::Queue<int> q{ ioc, 1 };
+
+    std::future<boost::optional<int>> fPop = q.asyncPop(boost::asio::use_future);
+    std::future<void> fPush = q.asyncPush(123, boost::asio::use_future);
+
+    ThreadPool{ ioc, 10 }.join();
+
+    BOOST_CHECK_NO_THROW(fPush.get());
+    BOOST_CHECK_EQUAL(123, *fPop.get());
+    BOOST_CHECK(q.empty());
+    BOOST_CHECK_EQUAL(0, q.cancel());
+
+
+    std::future<boost::optional<int>> fPopUnderflow = q.asyncPop(boost::asio::use_future);
+    BOOST_CHECK_EQUAL(1, q.cancel());
+
+    fPush = q.asyncPush(123, boost::asio::use_future);
+    BOOST_CHECK_EQUAL(0, q.cancel());
+
+    std::future<void> fPushOverflow = q.asyncPush(123, boost::asio::use_future);
+    BOOST_CHECK_EQUAL(1, q.cancel());
+
+    ioc.restart();
+    ThreadPool{ ioc, 10 }.join();
+
+    BOOST_CHECK_NO_THROW(fPush.get());
+    BOOST_CHECK_THROW(fPushOverflow.get(), boost::system::system_error);
+    BOOST_CHECK_THROW(fPopUnderflow.get(), boost::system::system_error);
+    BOOST_CHECK_EQUAL(1, q.size());
+    BOOST_CHECK_EQUAL(0, q.cancel());
+}
+
+BOOST_AUTO_TEST_CASE(contentTest)
+{
+    boost::asio::io_context ioc;
+    ba::async::Queue<std::size_t> q{ ioc, 10 };
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        for (std::size_t i = 1; i <= 10'000; ++i)
+            q.asyncPush(i, yield);
+    });
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        std::size_t sum = 0;
+        for (std::size_t i = 1; i <= 10'000; ++i)
+            BOOST_CHECK_NO_THROW(sum += q.asyncPop(yield).get());
+
+        BOOST_CHECK_EQUAL(50005000, sum);
+    });
+
+    ThreadPool{ ioc, 10 }.join();
+
+    BOOST_CHECK(q.empty());
+    BOOST_CHECK_EQUAL(0, q.cancel());
+}
+
+BOOST_AUTO_TEST_CASE(ManyProducerTest)
+{
+    boost::asio::io_context ioc;
+    ba::async::Queue<std::size_t> q{ioc, 15 };
+
+    for (int j = 0; j < 10; ++j)
+    {
+        boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+            for (std::size_t i = 1; i <= 1'000; ++i)
+                q.asyncPush(i, yield);
+        });
+    }
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        std::size_t sum = 0;
+        for (std::size_t i = 1; i <= 10'000; ++i)
+            BOOST_CHECK_NO_THROW(sum += q.asyncPop(yield).get());
+
+        BOOST_CHECK_EQUAL(5005000, sum);
+    });
+
+    ThreadPool{ ioc, 10 }.join();
+
+    BOOST_CHECK(q.empty());
+    BOOST_CHECK_EQUAL(0, q.cancel());
+}
+
+BOOST_AUTO_TEST_CASE(manyConsumerTest)
+{
+    boost::asio::io_context ioc;
+    ba::async::Queue<std::size_t> q{ ioc, 15 };
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        for (std::size_t i = 1; i <= 10'000; ++i)
+            BOOST_CHECK_NO_THROW(q.asyncPush(i, yield));
+    });
+
+    std::size_t sum = 0;
+
+    for (int j = 0; j < 10; ++j)
+    {
+        boost::asio::spawn(ioc, [&q, &sum](boost::asio::yield_context yield) {
+            for (std::size_t i = 1; i <= 1'000; ++i)
+                sum += q.asyncPop(yield).get();
+        });
+    }
+
+    ThreadPool{ ioc, 10 }.join();
+    BOOST_CHECK_EQUAL(50005000, sum);
+
+    BOOST_CHECK(q.empty());
+    BOOST_CHECK_EQUAL(0, q.cancel());
+}
+
+BOOST_AUTO_TEST_CASE(moveValueTest)
+{
+    struct Movable
+    {
+        Movable() = default;
+        Movable(Movable&&) = default;
+        Movable& operator=(Movable&&) = default;
+        Movable& operator=(const Movable&) = delete;
+
+        Movable(const Movable&)
+        {
+            BOOST_TEST_ERROR("Movement value test failed");
+        }
+    };
+
+    boost::asio::io_context ioc;
+    ba::async::Queue<Movable> q{ ioc, 10 };
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        for (std::size_t i = 1; i <= 1'000; ++i)
+            q.asyncPush(Movable{}, yield);
+    });
+
+    boost::asio::spawn(ioc, [&q](boost::asio::yield_context yield) {
+        for (std::size_t i = 1; i <= 1'000; ++i)
+            Movable m = std::move(q.asyncPop(yield).get());
+    });
+
+    ThreadPool{ ioc, 4 }.join();
+}
+
+BOOST_AUTO_TEST_CASE(moveQueueTest)
+{
+    boost::asio::io_context ioc;
+    ba::async::Queue<int> q1{ ioc, 2 };
+    ba::async::Queue<int> q2{ ioc, 10 };
+    std::vector<ba::async::Queue<int>> q3;
+   
+
+    q1.asyncPush(1, [](boost::system::error_code) {});
+    q1.asyncPush(2, [](boost::system::error_code) {});
+    q1.asyncPush(3, [](boost::system::error_code) {});
+    q1.asyncPush(4, [](boost::system::error_code) {});
+    q1.asyncPush(5, [](boost::system::error_code) {});
+
+    q1.asyncPop([&q1, &q2](boost::system::error_code, boost::optional<int>) mutable {
+        q2 = std::move(q1);
+    });
+
+    q1.asyncPop([&q2, &q3](boost::system::error_code, boost::optional<int>) mutable {
+        q3.push_back(std::move(q2));
+
+        BOOST_CHECK(q3[0].full());
+        BOOST_CHECK_EQUAL(1, q3[0].cancel());
+    });
+
+    ThreadPool{ ioc, 4 }.join();
+
+    BOOST_CHECK(q1.empty());
+    BOOST_CHECK_EQUAL(0, q1.cancel());
+
+    BOOST_CHECK(q2.empty());
+    BOOST_CHECK_EQUAL(0, q2.cancel());
+}

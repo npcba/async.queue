9e375f9ae9a53775d309244f01f77ac7eb0ae3a3
Victor Scherba <SherbaVA@pba.su>
2023-11-03 18:11:24 +0300

    CheckCallable, разрушающий Function::operator() и другие мелкие изменения.

--- include/async/queue/detail/check_callable.hpp
+++ include/async/queue/detail/check_callable.hpp
@@ -7,16 +7,20 @@ namespace ba {
 namespace async {
 namespace detail {
 
-template <typename F, typename Signature, typename Enable = void>
-struct CheckSignature : std::false_type {};
+template <typename F, typename Signature, typename EnableIf = void>
+struct CheckCallable : std::false_type {};
 
 template<typename F, typename R, typename... Args>
-struct CheckSignature<
+struct CheckCallable<
       F
     , R(Args...)
     , std::enable_if_t<
         std::is_convertible<
+#ifdef __cpp_lib_is_invocable
+              std::invoke_result_t<F&, Args...>
+#else
               std::result_of_t<F& (Args...)>
+#endif
             , R
             >::value ||
         std::is_void<R>::value

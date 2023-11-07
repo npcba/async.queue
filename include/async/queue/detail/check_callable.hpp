398c1e82a8051264d31dec28b63ba8ebbbe3954f
Victor Scherba <SherbaVA@pba.su>
2023-11-08 00:26:06 +0300

    Коментарии + мелкие исправления.

--- include/async/queue/detail/check_callable.hpp
+++ include/async/queue/detail/check_callable.hpp
@@ -7,9 +7,12 @@ namespace ba {
 namespace async {
 namespace detail {
 
+// Trait проверяет на возможность объекта типа F быть вызванным с аргументами типа Args.
+// По-умолчанию не может быть вызван (false).
 template <typename F, typename Signature, typename EnableIf = void>
 struct CheckCallable : std::false_type {};
 
+// Сециализация, для корректно-вызываемого F (true).
 template<typename F, typename R, typename... Args>
 struct CheckCallable<
       F
@@ -17,12 +20,12 @@ struct CheckCallable<
     , std::enable_if_t<
         std::is_convertible<
 #ifdef __cpp_lib_is_invocable
-              std::invoke_result_t<F&, Args...>
+              std::invoke_result_t<F&, Args...> // since C++17
 #else
-              std::result_of_t<F& (Args...)>
+              std::result_of_t<F& (Args...)>    // deprecated in C++17, deleted in C++20
 #endif
             , R
-            >::value ||
+            >::value ||                         // Результат либо конверируется в R, либо void.
         std::is_void<R>::value
         >
     >
--- include/async/queue/detail/function.hpp
+++ include/async/queue/detail/function.hpp
@@ -2,7 +2,6 @@
 
 #include <memory>
 #include <cassert>
-#include <functional>
 
 #include <boost/core/noncopyable.hpp>
 
@@ -11,6 +10,21 @@ namespace ba {
 namespace async {
 namespace detail {
 
+
+// Movable-only замена std::function для полиморфного хранения разнотипных хендлеров и связанных с ними аллокаторов
+// Возвращает только void, ибо в данной библиотеке другого не нужно (добавить не сложно).
+// Для распределения внутреннего состояния использует пользовательский аллокатор (для поддержки asio).
+// Сам аллокатор также хранится во внутреннем состоянии, т.к. верхушка не знает тип аллокатора.
+// Таким образом стрирается не только тип функции, но и тип аллокатора.
+// operator() вызывает внутренний объект и сразу разрушает его(повторный вызов запрещен,
+// в данной библиотеке он и невозможен).
+// Asio гарантирует своим пользователям, что allocator.deallocate будет вызван строго до вызова хендлера,
+// а также будет выставлен барьер памяти между allocate и deallocate если они разошлись по разным потокам.
+// Значит и данная библиотека должна поддерживать эту гарантию:
+// Очистка памяти производится до вызова operator() путем перемещения внутреннего состояния на стек,
+// удалением его из динамической памяти и вызовом стековой копии.
+// Барьер памяти между инициирующими операциями из параллельных потоков гарантируется мьютексами в Queue,
+// а между инициаторами и хендлерами барьер ставит asio::post. Поэтому явно его ставить не нужно.
 template <typename>
 class Function;
 
@@ -18,33 +32,44 @@ template <typename... Args>
 class Function<void(Args...)>
 {
 public:
+    // Умеет перемещаться.
     Function(Function&&) = default;
     Function& operator=(Function&&) = default;
 
+    // Не умеет копироваться.
     Function(const Function&) = delete;
     Function& operator=(const Function&) = delete;
 
+    // Отключены случайные перегрузки, т.к. в конструктор могут попасть универсальные ссылки,
+    // а нужны только rvalue-ref.
     template <typename F, typename Alloc>
     Function(F& f, const Alloc& a) = delete;
     template <typename F, typename Alloc>
     Function(const F& f, const Alloc& a) = delete;
 
+    // Инициализируется функтором по rvalue-ref и пользовательским аллокатором.
     template <typename F, typename Alloc>
     Function(F&& f, const Alloc& a)
     {
         using HolderType = Holder<F, Alloc>;
 
+        // Ребиндит пользовательский аллокатор на тип холдера.
         typename HolderType::allocator_type ha{ a };
+        // Выделяет память под холдер.
         HolderType* holder = ha.allocate(1);
+        // Стандартный аллокатор бросает исключение, нет гарантии, что пользовательский тоже,
+        // приведём поведение к общему.
         if (!holder)
             throw std::bad_alloc{};
 
         try
         {
+            // Конструирует холдер на выделенной памяти.
             new(holder) HolderType{ std::move(f), ha };
         }
         catch (...)
         {
+            // Если конструктор бросил исключение, освобождает память.
             ha.deallocate(holder, 1);
             throw;
         }
@@ -52,31 +77,35 @@ public:
         m_callable.reset(holder);
     }
 
+    // Пробрасывает вызов на внутренний объект
     void operator()(Args... args)
     {
+        // Это первый и единственный вызов operator(),
+        // повторного вызова в данной библиотеке нет,
+        // а если случился, то выбрасывайте ее, хендлеры более 1 раза не вызываются в asio.
         assert(m_callable);
-        if (!m_callable)
-            throw std::bad_function_call();
 
-        (*m_callable)(std::forward<Args>(args)...);
+        m_callable->destructibleCallOp(std::forward<Args>(args)...);
+        // После вызова, холдер самоуничтожился, отпускаем unique_ptr.
         m_callable.release();
     }
 
-    explicit operator bool() const noexcept
-    {
-        return m_callable;
-    }
-
 private:
+    // Виртуальный интерфейс.
     struct Callable
         : boost::noncopyable
     {
-        virtual void operator()(Args... args) = 0;
+        // Удаление внутреннего состояния и вызов внутреннего operator() (см. детали ниже).
+        virtual void destructibleCallOp(Args... args) = 0;
+        // Удаление внутреннего состояния без вызова operator() (для деструктора).
         virtual void destruct() = 0;
     protected:
+        // Деструктор вызывать запрещено, удаление происходит методами выше.
         ~Callable() = default;
     };
 
+    // Хранитель конкретного внутреннего состояния, параметризован пользовательским аллокатором,
+    // этим же аллокатором он и размещается в памяти.
     template <typename F, typename Alloc>
     class Holder
         : public Callable
@@ -85,11 +114,11 @@ private:
         using allocator_type = typename std::allocator_traits<Alloc>::template rebind_alloc<Holder>;
 
         Holder(F&& f, const allocator_type& a)
-            : m_f(std::move(f)), m_a{ a }
+            : m_f{ std::move(f) }, m_a{ a }
         {
         }
 
-        void operator()(Args... args) override
+        void destructibleCallOp(Args... args) override
         {
             doDestruct()(std::forward<Args>(args)...);
         }
@@ -100,14 +129,20 @@ private:
         }
 
     private:
+        // Перед удалением себя возвращает стековую копию внутреннего функтора (move-copy).
         F doDestruct()
         {
+            // Перемещает на стек функтор.
             F copyF = std::move(m_f);
+            // Копирует на стек аллокатор.
             allocator_type copyA = m_a;
 
+            // Деструктит себя.
             this->~Holder();
+            // Освобождает аллокатором память из-под себя.
             copyA.deallocate(this, 1);
 
+            // Возвращает копию функтора (аллокатор больше не нужен).
             return copyF;
         }
 
